02434287d9db7c170ea770dc8578c234b98dab1f
emre0han <yns.emre7100@gmail.com>
2024-03-03 23:26:10 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+String[] names= {"emre","ilayda","kadir","fırat","suzan" };
+int[] times= {100,200,300,400,500};
+Marathon m1=new Marathon(names,times);
+m1.displayAllMessages();
+}
+}
